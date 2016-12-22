cfa95461b786771c18b20724ad6073a75e744ef1
RedCyberSamurai <redcybersamurai@users.noreply.github.com>
2016-12-22 17:23:23 +0100

    function pointer exercise

--- methods/main.go
+++ methods/main.go
@@ -18,6 +18,7 @@ func (v Vertex) Abs() float64 {
  * -- Pointer Receiver --
  * Same as a Receiver, but takes no return type.
  * Instead it modifies values from the existent instance of the given type.
+ * Wether the given type is a pointer or not, the receiver pointer always grabs the pointer of the type.
  */
 func (v *Vertex) Scale(f float64) {
 	v.X *= f
@@ -37,6 +38,16 @@ func (f MyFloat) Abs() float64 {
 	return float64(f)
 }
 
+/**
+ * ScaleFunc(t Type, float64) => throws an error
+ *         ---^---
+ */
+func ScaleFunc(v *Vertex, f float64) {
+	v.X *= f
+	v.Y *= f
+}
+
+
 func main() {
 	fmt.Println("Print a value from a receiver")
 	f := MyFloat(-math.Sqrt2)
@@ -46,6 +57,13 @@ func main() {
 	v := Vertex{3, 4}
 	v.Scale(10) // {30, 40}
 	fmt.Println(v.Abs())
+
+	fmt.Println("Print a value from a function with pointer parameter")
+	p := Vertex{3, 4}
+	q := &Vertex{5, 6}
+	ScaleFunc(&p, 8)
+	ScaleFunc(q, 4)
+	fmt.Println(p, q)
 }
 
 // https://tour.golang.org/methods/5
