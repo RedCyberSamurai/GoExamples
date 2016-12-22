ce39b29650c9bb36279c2d32a09b3926c6174d7c
RedCyberSamurai <redcybersamurai@users.noreply.github.com>
2016-12-22 17:22:23 +0100

    modified string output * type switch

--- type-conversion/main.go
+++ type-conversion/main.go
@@ -4,6 +4,36 @@ import (
 	"fmt"
 )
 
+type IPAddr [4]byte
+
+/**
+ * This method will be overridden.
+ * While using the Print method of fmt,
+ * instead of printing an []IPAddr Array, it will print what the function returns.
+ * In this case an IP like x.x.x.x
+ */
+func (ip IPAddr) String() string {
+	numItems := len(ip)
+	if numItems == 4 {
+		return fmt.Sprintf("%v.%v.%v.%v", ip[0], ip[1], ip[2], ip[3])
+	}
+	return "0.0.0.0"
+}
+
+/**
+ * Switch is also available for types
+ */
+func do(i interface{}) {
+	switch v := i.(type) {
+	case int:
+		fmt.Printf("%d is an %T.\n", v, v)
+	case string:
+		fmt.Printf("%q is a %T.\n", v, v)
+	default:
+		fmt.Printf("Undefined Type >> %T << with value >> %v <<.\n", v, v)
+	}
+}
+
 func main() {
 	/**
 	 * Same as:
@@ -19,6 +49,7 @@ func main() {
 		u uint    = uint(f)
 	)
 
+	fmt.Println("--- Listing the types ---")
 	const p = "%T(%v) %T(%v) %T(%v)\n"
 	fmt.Printf(p, i, i, f, f, u, u)
 
@@ -30,4 +61,18 @@ func main() {
 	fmt.Printf("v2 is of type %T\n", v2)
 	fmt.Printf("v3 is of type %T\n", v3)
 	fmt.Printf("v4 is of type %T\n", v4)
+
+	fmt.Println("--- Using type switch ---")
+	do(42)
+	do("hello")
+	do(true)
+
+	fmt.Println("--- Using modified string conversion >fmt.Stringer::String()< ---")
+	hosts := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
+	}
 }
