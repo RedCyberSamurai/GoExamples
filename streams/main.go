017accfed668ba28bb1ed888beb45ef459493c26
RedCyberSamurai <redcybersamurai@users.noreply.github.com>
2017-01-11 18:08:28 +0100

    bytestream exercise rot13

--- streams/main.go
+++ streams/main.go
@@ -5,6 +5,8 @@ import (
 	"io"
 	"strings"
 	//"golang.org/x/tour/reader"
+	"golang.org/x/tour/reader"
+	"os"
 )
 
 type CustomReader struct{}
@@ -16,6 +18,33 @@ func (r CustomReader) Read(b []byte) (int, error) {
 	b[0] = 'A'
 	return 1, nil // bytestream length == 1
 				  // error will not give an EOF, thats why it will continue infinitly
+}
+
+
+type rot13Reader struct {
+	r io.Reader
+}
+
+func rot13(b byte) byte {
+	r := rune(b)
+
+	if r < 'n' && r >= 'a' || r < 'N' && r >= 'A' {
+		b += 13
+	} else if r >= 'n' && r <= 'z' || r >= 'N' && r <= 'Z' {
+		b -= 13
+	}
+
+	return b
+}
+
+func (rot rot13Reader) Read(b []byte) (out int, err error) {
+
+	out, err = rot.r.Read(b)
+	for i := 0; i < len(b); i++ {
+		b[i] = rot13(b[i])
+	}
+
+	return
 }
 
 func main() {
@@ -34,5 +63,11 @@ func main() {
 	}
 
 	fmt.Println("--- Manipulating bytestreams ---")
+	reader.Validate(CustomReader{})
 	// https://tour.golang.org/methods/22
+
+	fmt.Println("--- Override Reader for rot13 encryption ---")
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+	rotReader := rot13Reader{s}
+	io.Copy(os.Stdout, &rotReader)
 }
