6c0e5ef738d52f2138f754ce6465aa2f6d8dacad
RedCyberSamurai <redcybersamurai@users.noreply.github.com>
2017-01-12 19:37:15 +0100

    example for generating images

--- images/main.go
+++ images/main.go
@@ -3,7 +3,29 @@ package main
 import (
 	"fmt"
 	"image"
+	"image/color"
+	"golang.org/x/tour/pic"
 )
+
+type Image struct{
+	Width, Height int
+	Color uint8
+}
+
+// the dimension of the image
+func (i Image) Bounds() image.Rectangle {
+	return image.Rect(0, 0, i.Width, i.Height)
+}
+
+// the color type of the image
+func (i Image) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+// the color on a coordinate of the image
+func (i Image) At(x, y int) color.Color {
+	return color.RGBA{R:i.Color + uint8(x), G:i.Color + uint8(y), B:255, A:255}
+}
 
 func main() {
 
@@ -12,4 +34,8 @@ func main() {
 	fmt.Println(m.Bounds())
 	fmt.Println("--- Print image pixel rgba values ---")
 	fmt.Println(m.At(0,0).RGBA())
+
+	fmt.Println("--- Generating Image by definition ---")
+	m2 := Image{100, 100, 185}
+	pic.ShowImage(m2)
 }
